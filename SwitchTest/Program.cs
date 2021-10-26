0d38609186dc442a6c1479730dbbaaff5988522b
Mark Otway <mark@otway.com>
2021-10-26 22:44:36 +0100

    First commit

---
+++ SwitchTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SwitchTest
+{
+    class Program
+    {
+        private SampleEnum.Samples TheValue { get; set; }
+
+        private string Example => TheValue switch
+        {
+            SampleEnum.Samples.Sample1 => "Test 1",
+            SampleEnum.Samples.Sample2 => "Test 2",
+            SampleEnum.Samples.Sample3 => "Test 3",
+            SampleEnum.Samples.Sample4 => "Test 4"
+        };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
---
+++ SwitchTest/SampleEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace SwitchTest
+{
+    public class SampleEnum
+    {
+        public enum Samples
+        {
+            Sample1,
+            Sample2,
+            Sample3,
+            Sample4
+        }
+    }
+}
+
